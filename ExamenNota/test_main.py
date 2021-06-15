621e632d4dbca67baa2927bb1efc7772392e0919
Alberto <albertobulpemartinez@gmail.com>
2021-06-15 09:16:52 +0200

    Variables propias añadidos a los Test.

--- ExamenNota/test_main.py
+++ ExamenNota/test_main.py
@@ -24,26 +24,41 @@ hoyo = 1
 par = 3
 golpes = 7
 def test_modifica_par():
-    modifica_par(tabla,hoyo,par)
-    for element in tabla:
+    tabla1 = [{'hoyo': 1, 'par': 0, 'golpes': 0}]
+    par1 = 2
+    hoyo1 = 1
+    modifica_par(tabla1,hoyo1,par1)
+    for element in tabla1:
         if element['hoyo'] == 1:
-            assert element['par'] == 0
+            assert element['par'] == 2
 
 
 
 def test_modifica_golpes():
-    modifica_golpes(tabla,hoyo,golpes)
-    for element in tabla:
+    tabla1 = [{'hoyo': 1, 'par': 1, 'golpes': 3}]
+    par1 = 2
+    hoyo1 = 1
+    golpes1 = 5
+    modifica_golpes(tabla1,hoyo1,golpes1)
+    for element in tabla1:
         if element['hoyo'] == 1:
-            assert element['golpes'] == 7
+            assert element['golpes'] == 5
 
 def test_resultado():
-    assert resultado(tabla,10) == -6
-    assert resultado(tabla,14) == 'PAR'
-    assert resultado(tabla,17) == 'EAGLE'
-    assert resultado(tabla,18) == 'BIRDIE'
-    assert resultado(tabla,15) == 'DOBLE BOGEY'
-    assert resultado(tabla,16) == 'BOGEY'
+    tabla1 = [{'hoyo': 16, 'par': 4, 'golpes': 6},
+              {'hoyo': 10, 'par': 4, 'golpes': 10},
+              {'hoyo': 14, 'par': 4, 'golpes': 5},
+              {'hoyo': 17, 'par': 3, 'golpes': 3},
+              {'hoyo': 18, 'par': 4, 'golpes': 3},
+              {'hoyo': 15, 'par': 5, 'golpes': 3},
+              ]
+
+    assert resultado(tabla1,10) == -6
+    assert resultado(tabla1,14) == 'BOGEY'
+    assert resultado(tabla1,17) == 'PAR'
+    assert resultado(tabla1,18) == 'BIRDIE'
+    assert resultado(tabla1,15) == 'EAGLE'
+    assert resultado(tabla1,16) == 'DOBLE BOGEY'
 
 
 
